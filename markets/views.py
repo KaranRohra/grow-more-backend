7037abcba309f95d2c8fc0023460f47864ad8622
khushboo bajaj <khushboobajaj2572002@gmail.com>
2022-10-25 16:13:23 +0530

    [REF #3]: formatted

--- markets/views.py
+++ markets/views.py
@@ -2,22 +2,21 @@ from django.shortcuts import HttpResponse
 import yfinance
 from rest_framework import views
 
+
 class GetHistoricalAPI(views.APIView):
-    def get(self, request,*args, **kwargs):
+    def get(self, request, *args, **kwargs):
         symbol = kwargs["symbol"]
         interval = kwargs["interval"]
         range = kwargs["range"]
-
         script = yfinance.Ticker(symbol)
-
         df = script.history(period=range, interval=interval)
         df = df[["Open", "High", "Low", "Close", "Volume"]].to_dict("split")
         timestamp = df["index"]
         columns = df["columns"]
         data = df["data"]
-        context={
-            "timestamp" : timestamp[0],
-            "data":data[0],
-            "columns":columns
+        context = {
+            "timestamp": timestamp[0],
+            "data": data[0],
+            "columns": columns
         }
         return HttpResponse(context)
