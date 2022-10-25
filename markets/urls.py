86cce633deeb13b0973244b3138ec1fcab6d134f
khushboo bajaj <khushboobajaj2572002@gmail.com>
2022-10-25 15:25:47 +0530

    [REF #3]:completed historical views

---
+++ markets/urls.py
@@ -0,0 +1,6 @@
+from markets import views
+from django.urls import path
+
+urlpatterns = [
+    path("historical-data/<str:symbol>/<str:interval>/<str:range>",views.GetHistoricalAPI.as_view(),name="historical data")
+]
--- markets/views.py
+++ markets/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+import yfinance
+from rest_framework import views
 # Create your views here.
+class GetHistoricalAPI(views.APIView):
+    def get(self, request, **kwargs):
+        symbol = kwargs["symbol"]
+        interval = kwargs["interval"]
+        range = kwargs["range"]
+
+        script = yfinance.Ticker(symbol)
+
+        df = script.history(period=range, interval=interval)
+        df = df[["Open", "High", "Low", "Close", "Volume"]].to_dict("split")
+        timestamp = df["index"]
+        columns = df["columns"]
+        data = df["data"]
+        context={
+            "timestamp" : timestamp[0],
+            "data":data[0],
+            "columns":columns
+        }
+        return HttpResponse(context)
