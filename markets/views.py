acbbd1fd335385f26b1bdcfc27d7bf4139e18d75
KaranRohra <rohrakaran38@gmail.com>
2022-10-25 16:28:31 +0530

    [REF #3]: Updated historical api

--- markets/views.py
+++ markets/views.py
@@ -1,23 +1,20 @@
-from django.shortcuts import HttpResponse
+from rest_framework.response import Response
 import yfinance
 from rest_framework import views
 
+
 class GetHistoricalAPI(views.APIView):
-    def get(self, request,*args, **kwargs):
+    def get(self, request, *args, **kwargs):
         symbol = kwargs["symbol"]
-        interval = kwargs["interval"]
-        range = kwargs["range"]
+        interval = request.query_params["interval"]
+        range = request.query_params["range"]
 
         script = yfinance.Ticker(symbol)
 
         df = script.history(period=range, interval=interval)
-        df = df[["Open", "High", "Low", "Close", "Volume"]].to_dict("split")
-        timestamp = df["index"]
-        columns = df["columns"]
-        data = df["data"]
-        context={
-            "timestamp" : timestamp[0],
-            "data":data[0],
-            "columns":columns
-        }
-        return HttpResponse(context)
+        df = df[["Open", "High", "Low", "Close"]].to_dict("split")
+        return Response(
+            {"timestamp": df["index"], "ohlc": df["data"], "columns": df["columns"]}
+        )
+
+
