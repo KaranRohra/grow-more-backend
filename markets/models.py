8798de3f9cc8004ff5309618b96646a2f79dcd7e
khushboo bajaj <khushboobajaj2572002@gmail.com>
2022-10-26 11:38:50 +0530

    made suggested changes

--- markets/models.py
+++ markets/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 
-
 class Stock(models.Model):
     name = models.CharField(max_length=20)
     symbol = models.CharField(max_length=20)
 
-
 class ProfitAndLoss(models.Model):
     symbol = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    year = models.CharField(max_length=20)
-    revenue = models.CharField(max_length=20)
-    expenses = models.CharField(max_length=20)
-    operating_profit = models.CharField(max_length=20)
-    opm = models.CharField(max_length=20)
-    other_income = models.CharField(max_length=20)
-    interest = models.CharField(max_length=20)
-    depreciation = models.CharField(max_length=20)
-    profit_before_tax = models.CharField(max_length=20)
-    tax = models.CharField(max_length=20)
-    net_profit = models.CharField(max_length=20)
-    eps = models.CharField(max_length=20)
-    dividend_payout = models.CharField(max_length=20,blank=True,null=True)
+    year = models.CharField(max_length=32)
+    revenue = models.CharField(max_length=128)
+    expenses = models.CharField(max_length=128)
+    operating_profit = models.CharField(max_length=128)
+    opm = models.CharField(max_length=128)
+    other_income = models.CharField(max_length=128)
+    interest = models.CharField(max_length=128)
+    depreciation = models.CharField(max_length=128)
+    profit_before_tax = models.CharField(max_length=128)
+    tax = models.CharField(max_length=128)
+    net_profit = models.CharField(max_length=128)
+    eps = models.CharField(max_length=128)
+    dividend_payout = models.CharField(max_length=128,blank=True,null=True)
