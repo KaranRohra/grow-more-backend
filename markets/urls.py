b1282e07a99370aa13cedd8283251d221b7e22c2
anishadhameja <ani.dhameja@gmail.com>
2022-10-27 17:25:50 +0530

    [REF #5] : quarterly results url changed

--- markets/urls.py
+++ markets/urls.py
@@ -4,5 +4,5 @@ from markets import views
 
 urlpatterns = [
     path("historical/<str:symbol>/", views.GetHistoricalAPI.as_view(), name="historical-data"),
-    path("quaterly-result/<str:symbol>/", views.GetQuarterlyResultsAPI.as_view(), name="quarterly-result")
+    path("<str:symbol>/quarterly-result/", views.GetQuarterlyResultsAPI.as_view(), name="quarterly-result")
 ]
