8128c462c1dd8d491e2367620e9b1e936f70b8c6
khushboo bajaj <khushboobajaj2572002@gmail.com>
2022-11-23 17:39:10 +0530

    [REF #14]:implemented the changes suggested

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-import yfinance
+from nsetools import Nse
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework import authentication
@@ -6,7 +6,6 @@ from rest_framework import permissions
 
 from portfolio import models
 from markets import models as market_models
-from portfolio import serializers
 
 
 class HoldingAPI(views.APIView):
@@ -14,37 +13,46 @@ class HoldingAPI(views.APIView):
     authentication_classes = (authentication.TokenAuthentication,)
 
     def get(self, request):
-        portfolio = models.Holding.objects.filter(
+        holdings = models.Holding.objects.filter(
             user=request.user).order_by("stock")
-        data = serializers.HoldingSerializer(portfolio, many=True).data
-        return Response(self.get_portfolio(data))
+        return Response(self.get_holdings(holdings))
 
     def post(self, request):
         orderType = request.POST["type"]
         quantity = int(request.POST["quantity"])
         stock = market_models.Stock.objects.get(
             nse_symbol=request.POST["nse_symbol"])
-        script = yfinance.Ticker(stock.nse_symbol+".NS").info
-        price = script["regularMarketPrice"]
-
-        if orderType == "buy":
+        nse = Nse()
+        detail = nse.get_quote(stock.nse_symbol)
+        price = detail["lastPrice"]
+        message = ""
+        if orderType == "BUY":
             models.Holding.objects.create(user=request.user,
-                price=price, quantity=quantity, stock=stock)
-            return Response({"message": "Your order is placed"})
-        else:
+                                          price=price, quantity=quantity, stock=stock)
+            message = "Your order is placed"
+        elif orderType == "SELL":
             holding_data = models.Holding.objects.filter(
                 stock=stock).order_by("created_at")
             message = self.sell_stock(holding_data, quantity)
+        else:
+            message = "Invalid order type"
+
+        sum, total_quantity = 0, 0
+        holding_data = models.Holding.objects.filter(
+            user=request.user, stock=stock)
+        if len(holding_data) == 0:
             return Response({"message": message})
+        for holding in holding_data:
+            sum += holding.price*holding.quantity
+            total_quantity += holding.quantity
+        return Response({"holding": {"price": round(sum/total_quantity, 2), "quantity": total_quantity, "symbol": stock.nse_symbol}, "message": message})
 
     def sell_stock(self, holding_data, quantity):
-        message = ""
         total_quantity = sum(
             [holding.quantity for holding in holding_data])
 
         if quantity > total_quantity:
-            message = "You do not have "+str(quantity)+" shares"
-            return message
+            return "You do not have "+str(quantity)+" shares"
 
         for holdings in holding_data:
             current_quantity = holdings.quantity
@@ -59,13 +67,13 @@ class HoldingAPI(views.APIView):
         message = "Your order is placed"
         return message
 
-    def get_portfolio(self, data):
-        symbol, holding, quantity, sum = data[0]["stock"]["nse_symbol"], 0, 0, 0
+    def get_holdings(self, holdings):
+        symbol, quantity, sum = holdings.first().stock.nse_symbol, 0, 0
         context = []
-        for holding in data:
-            if holding["stock"]["nse_symbol"] == symbol:
-                sum += holding["quantity"]*holding["price"]
-                quantity += holding["quantity"]
+        for holding in holdings:
+            if holding.stock.nse_symbol == symbol:
+                sum += holding.quantity*holding.price
+                quantity += holding.quantity
             else:
                 context.append(
                     {
@@ -74,9 +82,9 @@ class HoldingAPI(views.APIView):
                         "price": round(sum/quantity, 2)
                     }
                 )
-                sum, quantity = holding["quantity"] * \
-                    holding["price"], holding["quantity"]
-                symbol = holding["stock"]["nse_symbol"]
+                sum, quantity = holding.quantity * \
+                    holding.price, holding.quantity
+                symbol = holding.stock.nse_symbol
 
         context.append(
             {
