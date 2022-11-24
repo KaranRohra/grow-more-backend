35a169d1c6b9ed18238a813aa5b7a95f912ef22c
KaranRohra <rohrakaran38@gmail.com>
2022-11-24 14:36:37 +0530

    [REF #14]: Updated and formated

--- portfolio/views.py
+++ portfolio/views.py
@@ -15,14 +15,14 @@ class HoldingAPI(views.APIView):
     authentication_classes = (authentication.TokenAuthentication,)
 
     def get(self, request):
-        holdings = models.Holding.objects.filter(
-            user=request.user).order_by("stock", "-created_at")
+        holdings = models.Holding.objects.filter(user=request.user).order_by(
+            "stock", "-created_at"
+        )
         return Response(self.get_holdings(holdings))
 
     def post(self, request):
         quantity = int(request.POST["quantity"])
-        stock = market_models.Stock.objects.get(
-            nse_symbol=request.POST["nse_symbol"])
+        stock = market_models.Stock.objects.get(nse_symbol=request.POST["nse_symbol"])
         price = nse.get_quote(stock.nse_symbol)["lastPrice"]
         message = None
         if request.POST["type"] == "BUY":
@@ -41,8 +41,7 @@ class HoldingAPI(views.APIView):
             message = "Invalid order type"
 
         sum, total_quantity = 0, 0
-        holding_data = models.Holding.objects.filter(
-            user=request.user, stock=stock)
+        holding_data = models.Holding.objects.filter(user=request.user, stock=stock)
         if len(holding_data) == 0:
             return Response({"message": message})
         for holding in holding_data:
@@ -78,29 +77,28 @@ class HoldingAPI(views.APIView):
         return "Sell order is placed"
 
     def get_holdings(self, holdings):
-        symbol, quantity, sum = holdings.first().stock.nse_symbol, 0, 0
+        symbol, quantity, sum = holdings[0].stock.nse_symbol, 0, 0
         context = []
 
         for i in range(len(holdings)):
             holding = holdings[i]
-            if holding.stock.nse_symbol != symbol or i == len(holdings)-1:
+            if holding.stock.nse_symbol != symbol or i == len(holdings) - 1:
                 ltp = nse.get_quote(symbol)["lastPrice"]
-                avg_price = round(sum/quantity, 2)
+                avg_price = round(sum / quantity, 2)
                 context.append(
                     {
                         "Symbol": symbol,
                         "Quantity": quantity,
                         "Avg Price": avg_price,
                         "LTP": round(ltp, 2),
-                        "Current Value": round(ltp*quantity, 2),
-                        "P&L": round((ltp-avg_price)*quantity, 2),
-                        "Net Change": round((ltp-avg_price)*100/avg_price, 2)
+                        "Current Value": round(ltp * quantity, 2),
+                        "P&L": round((ltp - avg_price) * quantity, 2),
+                        "Net Change": round((ltp - avg_price) * 100 / avg_price, 2),
                     }
                 )
-                sum, quantity = holding.quantity * holding.price, holding.quantity
-                symbol = holding.stock.nse_symbol
-            else:
-                sum += holding.quantity * holding.price
-                quantity += holding.quantity
+                symbol, sum, quantity = holding.stock.nse_symbol, 0, 0, 0
+
+            sum += holding.quantity * holding.price
+            quantity += holding.quantity
 
         return context
