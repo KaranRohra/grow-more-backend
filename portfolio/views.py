20a0a2a93c9d8bc24178c496765a4f9ffe12fad2
khushboo bajaj <khushboobajaj2572002@gmail.com>
2022-11-24 14:26:04 +0530

    [REF #14]:implemented ltp p&l and netchange

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,17 +9,20 @@ from markets import models as market_models
 
 nse = Nse()
 
+
 class HoldingAPI(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
     authentication_classes = (authentication.TokenAuthentication,)
 
     def get(self, request):
-        holdings = models.Holding.objects.filter(user=request.user).order_by("stock")
+        holdings = models.Holding.objects.filter(
+            user=request.user).order_by("stock", "-created_at")
         return Response(self.get_holdings(holdings))
 
     def post(self, request):
         quantity = int(request.POST["quantity"])
-        stock = market_models.Stock.objects.get(nse_symbol=request.POST["nse_symbol"])
+        stock = market_models.Stock.objects.get(
+            nse_symbol=request.POST["nse_symbol"])
         price = nse.get_quote(stock.nse_symbol)["lastPrice"]
         message = None
         if request.POST["type"] == "BUY":
@@ -38,7 +41,8 @@ class HoldingAPI(views.APIView):
             message = "Invalid order type"
 
         sum, total_quantity = 0, 0
-        holding_data = models.Holding.objects.filter(user=request.user, stock=stock)
+        holding_data = models.Holding.objects.filter(
+            user=request.user, stock=stock)
         if len(holding_data) == 0:
             return Response({"message": message})
         for holding in holding_data:
@@ -76,23 +80,27 @@ class HoldingAPI(views.APIView):
     def get_holdings(self, holdings):
         symbol, quantity, sum = holdings.first().stock.nse_symbol, 0, 0
         context = []
-        for holding in holdings:
-            if holding.stock.nse_symbol == symbol:
-                sum += holding.quantity * holding.price
-                quantity += holding.quantity
-            else:
+
+        for i in range(len(holdings)):
+            holding = holdings[i]
+            if holding.stock.nse_symbol != symbol or i == len(holdings)-1:
+                ltp = nse.get_quote(symbol)["lastPrice"]
+                avg_price = round(sum/quantity, 2)
                 context.append(
                     {
-                        "symbol": symbol,
-                        "quantity": quantity,
-                        "price": round(sum / quantity, 2),
+                        "Symbol": symbol,
+                        "Quantity": quantity,
+                        "Avg Price": avg_price,
+                        "LTP": round(ltp, 2),
+                        "Current Value": round(ltp*quantity, 2),
+                        "P&L": round((ltp-avg_price)*quantity, 2),
+                        "Net Change": round((ltp-avg_price)*100/avg_price, 2)
                     }
                 )
                 sum, quantity = holding.quantity * holding.price, holding.quantity
                 symbol = holding.stock.nse_symbol
-
-        context.append(
-            {"symbol": symbol, "quantity": quantity, "price": round(sum / quantity, 2)}
-        )
+            else:
+                sum += holding.quantity * holding.price
+                quantity += holding.quantity
 
         return context
